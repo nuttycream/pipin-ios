47a536eb15dc580c0a18227e95ce0472b3d20b64
nuttycream <issanutty@gmail.com>
2025-05-10 13:01:28 -0700

    chore: initial

---
+++ pipin/pipinApp.swift
@@ -0,0 +1,17 @@
+//
+//  pipinApp.swift
+//  pipin
+//
+//  Created by John on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct pipinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pipinTests/pipinTests.swift
@@ -0,0 +1,16 @@
+//
+//  pipinTests.swift
+//  pipinTests
+//
+//  Created by John on 5/10/25.
+//
+
+import Testing
+
+struct pipinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
