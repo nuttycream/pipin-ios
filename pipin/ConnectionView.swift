cd734c2596031f755649d21a99953a84936fc3cc
nuttycream <issanutty@gmail.com>
2025-05-11 19:18:04 -0700

    feat: implement connectionView onConnect logic

--- pipin/ConnectionView.swift
+++ pipin/ConnectionView.swift
@@ -10,11 +10,13 @@ struct ConnectionView: View {
     @Binding var isPresented: Bool
     @Binding var ipAddress: String
     @Binding var port: String
-
+    var onConnect: () -> Void
+    var onDisconnect: () -> Void
+    var isConnected: Bool
+    
     var body: some View {
         VStack {
             HStack {
-                
                 Button(action: {
                     isPresented = false
                 }) {
@@ -34,32 +36,42 @@ struct ConnectionView: View {
                 TextField("IP Address", text: $ipAddress)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal)
+                    .disableAutocorrection(true)
+                    // this errors out on mac target
+                    //.keyboardType(.decimalPad)
 
                 TextField("Port", text: $port)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal)
+                    //.keyboardType(.numberPad)
 
-                Button("Submit") {
-                    // Handle connection logic here
-                    isPresented = false
+                if isConnected {
+                    Button("Disconnect") {
+                        onDisconnect()
+                        isPresented = false
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                } else {
+                    Button("Connect") {
+                        onConnect()
+                        isPresented = false
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .disabled(ipAddress.isEmpty || port.isEmpty)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .padding(.horizontal)
             }
+            
             Spacer()
-        }.frame(minWidth: 300) 
+        }.frame(minWidth: 300)
     }
-}
-
-#Preview {
-    // Provide dummy state values for preview
-    ConnectionView(
-        isPresented: .constant(true),
-        ipAddress: .constant("192.168.0.1"),
-        port: .constant("8080")
-    )
 }
